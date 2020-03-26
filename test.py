bcf3870a514994dfa900da54b08ecaebd0c48349
duchoud <duchonic@bluewin.ch>
2020-03-26 13:50:47 +0000

    update2

---
+++ test.py
@@ -0,0 +1,48 @@
+import serial
+import threading
+import time
+
+
+def readFromPort(ser):
+    while True:
+        if ser.in_waiting > 0:
+            print("rx: ", ser.readline())
+
+
+def writeToPort(ser):
+    while True:
+        command = input()
+        ser.write(str.encode('<'+command+'>'))
+
+
+def simulation(ser):
+    time.sleep(4)
+    send = '<1>'
+    ser.write(str.encode(send + '\n'))
+    print('tx: ' + send)
+    time.sleep(2)
+    while 1:
+        send = '<t1 13 10 1>\n'
+        ser.write(str.encode(send))
+        time.sleep(3)
+        send = '<t1 13 5 0>\n'
+        ser.write(str.encode(send))
+        time.sleep(4)
+        send = '<0>\n'
+        ser.write(str.encode(send))
+        time.sleep(60)
+        send = '<1>\n'
+        ser.write(str.encode(send))
+
+
+
+ser = serial.Serial("/dev/ttyACM0", baudrate=115200, timeout=1)
+
+thread_rx = threading.Thread( target=readFromPort, args=(ser,) )
+thread_tx = threading.Thread( target=writeToPort, args=(ser,) )
+thread_simulation = threading.Thread( target=simulation, args=(ser,) )
+
+thread_rx.start()
+thread_tx.start()
+thread_simulation.start()
+
